b503b499b31c37957254f37dc0010d7619234088
ratnadip1998 <40835957+ratnadip1998@users.noreply.github.com>
2022-08-30 02:23:13 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,30 @@
+# Quick Geolocation Search
+
+
+<img alt="GitHub stars" src="https://img.shields.io/github/stars/cipher387/quickgeolocationsearch">
+ 
+<img alt="GitHub forks" src="https://img.shields.io/github/forks/cipher387/quickgeolocationsearch">
+
+[![contributions welcome](https://img.shields.io/badge/contributions-welcome-brightgreen.svg?style=flat)](https://github.com/dwyl/esta/issues)
+    <p align="center">
+
+
+[![Hits](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fcipher387%2Fquickgeolocationsearch%2F&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false)](https://hits.seeyoufarm.com)
+
+This simple tool helps save a lot of time when gathering information about geolocation. Enter latitude and longitude once and click the buttons with the names of online maps, to quickly jump to it.
+
+https://cipher387.github.io/quickgeolocationsearch/
+
+<hr>
+
+Twitter — https://twitter.com/cyb_detective
+
+Telegram — https://t.me/cybdetective
+
+Revue — https://www.getrevue.co/profile/cyb_detective
+
+Github — https://github.com/cipher387
+
+Discord — https://cipher387.github.io/discordlink.html
+
+<hr>
---
+++ script.js
@@ -0,0 +1,66 @@
+ymaps = window.ymaps;
+ymaps.ready(init);
+
+function init() {
+    var myMap = new ymaps.Map("map", {
+        center: [35.00, 1.00],
+        zoom: 4
+    }, {
+        searchControlProvider: 'yandex#search'
+    });
+myMap.events.add('click', function (e) {
+        if (!myMap.balloon.isOpen()) {
+            var coords = e.get('coords');
+		document.getElementById("latitude").value = [coords[0].toPrecision(9)];
+		document.getElementById("longitude").value = [coords[1].toPrecision(9)];
+            myMap.balloon.open(coords, {
+                contentHeader:'Coordinates:',
+                contentBody:'<p>' + [
+                    coords[0].toPrecision(6),
+                    coords[1].toPrecision(6)
+                    ].join(', ') + '</p>'
+            });
+        }
+        else {
+            myMap.balloon.close();
+        }
+    });
+
+}
+
+function buttonOpen(buttonValue) {
+
+    latitude = Number(document.getElementById("latitude").value).toFixed(4);
+    longitude = Number(document.getElementById("longitude").value).toFixed(4);
+	
+
+    const links = [];
+    links["YandexMaps"] = "https://maps.yandex.com/?ll="+longitude+","+latitude+"&pt="+longitude+","+latitude;
+    links["Twitter"] = "https://twitter.com/search?q=near%3A"+latitude+"%2C"+longitude+"&src=typed_query&f=live";
+    links["Onlyfinder"] = "https://onlyfinder.com/location%3a"+latitude+"%2c"+longitude+"%2c100km/profiles/";
+    links["Ventusky"] = "https://www.ventusky.com/"+latitude+";"+longitude;
+    links["Strava"] = "https://labs.strava.com/heatmap/#10.86/"+longitude+"/"+latitude+"/hot/all";
+    links["Wikimapia"] = "http://wikimapia.org/#lang=en&lat="+latitude+"&lon="+longitude+"&z=15&m=b";
+    links["Pastvu"] = "https://pastvu.com?g="+latitude+","+longitude+"&z=16&s=osm&t=mapnik&type=1";
+    links["LightPollutionMap"] = "https://www.lightpollutionmap.info/#zoom=10.00&lat="+latitude+"&lon="+longitude+"&layers=B0FFFFFFFTFFFFFFFFFF";
+    links["Skyvector"] = "https://skyvector.com/?ll="+latitude+","+longitude+"&chart=301&zoom=6";
+    links["Emporis"] = "https://www.emporis.com/buildings/map#c="+latitude+","+longitude+"&z=16";
+    links["Opencellid"] = "https://opencellid.org/#zoom=13&lat="+latitude+"&lon="+longitude;
+    links["GoogleMaps"] = "https://www.google.com/maps/@"+latitude+","+longitude+",12.12z";
+    links["ShadowMap"] = "https://app.shadowmap.org/?lat="+latitude+"&lng="+longitude+"&zoom=16&basemap=map&time=1654604735200&vq=2";
+    links["SunCalc"] = "https://www.suncalc.org/#/"+latitude+","+longitude+",3/2022.06.07/15:53/1/3";
+    links["SentinelHub"] = "https://apps.sentinel-hub.com/sentinel-playground/?source=S2L2A&lat="+latitude+"&lng="+longitude+"&zoom=12&preset=1_TRUE_COLOR&layers=B01,B02,B03&maxcc=20&gain=1.0&gamma=1.0&time=2021-06-01%7C2021-12-06&atmFilter=&showDates=false";
+    links["Copernix"] = "https://copernix.io/#?where="+longitude+","+latitude+",9&?query=&?map_type=hybrid";
+    links["Openstreetmap"] = "https://www.openstreetmap.org/#map=10/"+latitude+"/"+longitude;
+    links["YouTube"] = "https://mattw.io/youtube-geofind/location?location="+latitude+","+longitude+"&doSearch=true";
+
+    window.open(links[buttonValue]);
+
+}
+
+
+ 
+   
+
+
+
